6bcecd5b1bd3dbb74c273c90261ce60fa97bd7c3
Yanni Sun <yanni.sun@wexinc.com>
2026-07-02 11:39:41 +1000

    Revert "Add Banking API: credit cards, transactions, Treasury FX integration, PostgreSQL + Docker"
    This reverts commit de5c4071f46bad369af11226515854c8cb6b00c2.

--- BankingApi/Models/Responses/CardResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace BankingApi.Models.Responses;
-
-public class CardResponse
-{
-    public Guid Id { get; set; }
-    public decimal CreditLimit { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
--- BankingApi/Repositories/Contracts/ITransactionRepository.cs
+++
@@ -1,9 +0,0 @@
-using BankingApi.Domain;
-
-namespace BankingApi.Repositories.Contracts;
-
-public interface ITransactionRepository
-{
-    Task<Transaction> AddAsync(Transaction transaction);
-    Task<Transaction?> GetByIdAsync(Guid id);
-}
